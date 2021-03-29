0162d6b0ce083e9b57fb99e8b340b79fd222234c
JJdoubleA <jaytdot2k@gmail.com>
2021-03-29 11:49:19 -0400

    deploy8

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.16.0"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "JeremyAugustin"
 set :repo_url, "https://github.com/JJdoubleA/Elevators_Consolidation_App"
-
+set :default_branch, "main"
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
